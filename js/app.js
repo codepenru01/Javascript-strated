1f53b5b4c69d96e77b2edc9df22b4f933445e10e
codepenru01 <codepenru01@gmail.com>
2022-07-29 09:32:56 +0100

    Homework 4

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 var intro = alert("Xush kelibsiz!");
-console.log(prompt("Familyangiz ?"));
-console.log(prompt("Ismingiz ?"));
-console.log(prompt("Qayerdansiz"));
-console.log(prompt("Telefon raqamingiz ?"));
-console.log(prompt("Dadangizni ismlari ?"));
-console.log(prompt("Onangizni ismlari ?"));
-console.log(prompt("Qayerda o'qiysiz ?"));
-console.log(confirm("Sizga saytimiz yoqdimi ?"));
+console.log("F.I.SH: " + prompt("F.I.SH ?"));
+console.log("Yoshi: " + prompt("Yoshingiz nechida ?"));
+console.log("Qayerdanligi: " + prompt("Qayerdansiz ?"));
+console.log("Qayerda ta'lim olish " + prompt("Qayerda ta'lim olasiz, yoki ishlaysiz ?"));
+console.log("Oilaviy muhiti: " + prompt("Oilaviy muhit ?"));
+console.log("Tel raqami: " + prompt("Tel raqamingiz ?"));
+console.log("Email: " + prompt("Elektron pochtangiz(email,gmail,mail,yandex)"));
+console.log("Turar joy: " + prompt("Turar joyiz ?"));
+console.log("Poshta indexsi: " + prompt("Pochta indeksi ?"));
 
 var elMode = document.querySelector(".btn__mode");
 var elBody = document.querySelector("body");
